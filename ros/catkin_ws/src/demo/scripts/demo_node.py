5a05f5c0d17dfe78d12e7dc1973a639f86466c4b
Andrey Koptug <domasedd@yandex.ru>
2020-01-18 16:46:45 +0200

    Added speed and lines recognition

--- ros/catkin_ws/src/demo/scripts/demo_node.py
+++ ros/catkin_ws/src/demo/scripts/demo_node.py
@@ -4,6 +4,63 @@ from duckietown_msgs.msg import Twist2DStamped
 from sensor_msgs.msg import CompressedImage, Image
 import rospy
 
+#!/usr/bin/env python
+import cv_bridge
+from cv_bridge import CvBridge, CvBridgeError
+from duckietown_msgs.msg import Twist2DStamped
+from sensor_msgs.msg import CompressedImage, Image
+import rospy
+import numpy as np
+import math
+import sys
+sys.path.append(sys.path.pop(1))
+sys.path.append(sys.path.pop(1))
+import cv2
+
+def calc_angle(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    height = image.shape[0]
+    width = image.shape[1]
+
+    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    canny_image = cv2.Canny(gray_image, 100, 200)
+
+
+    lines = cv2.HoughLinesP(canny_image,
+                            rho=1,
+                            theta=np.pi / 180,
+                            threshold=100,
+                            lines=np.array([]),
+                            minLineLength=100,
+                            maxLineGap=10)
+
+    #TODO:  Come up with an algorithm that will process the lines
+
+    return 0.0;
+
+
+def img_to_cv2(image_msg):
+    """
+    Convert the image message into a cv2 image (numpy.ndarray)
+    to be able to do OpenCV operations in it.
+    :param Image or CompressedImage image_msg: the message to transform
+    """
+    type_as_str = str(type(image_msg))
+    if type_as_str.find('sensor_msgs.msg._CompressedImage.CompressedImage') >= 0:
+        np_arr = np.fromstring(image_msg.data, np.uint8)
+        return cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    elif type_as_str.find('sensor_msgs.msg._Image.Image') >= 0:
+        try:
+            return self.bridge.imgmsg_to_cv2(image_msg, image_msg.encoding)
+        except CvBridgeError as e:
+            rospy.logerr("Error when converting image: " + str(e))
+            return None
+        else:
+            rospy.logerr("We don't know how to transform image of type " + str(type(image_msg)))
+            return None
+
+
 class DemoNode(object):
     def __init__(self):
         self.node_name = "LineDetectorNode"
@@ -13,8 +70,8 @@ class DemoNode(object):
 
     def cbImage(self, image_msg):
         msg = Twist2DStamped()
-        msg.v = 0.0
-        msg.omega = 0.5
+        msg.v = 0.3
+        msg.omega = calc_angle(img_to_cv2(image_msg))
         self.pub_cmd.publish(msg)
 
 if __name__ == '__main__': 
